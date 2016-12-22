0cb8f3373c51c1ab40031283df08e2b71cd3bef1
l0rb <lorbritzer@yahoo.de>
2016-12-22 22:28:27 +0100

    day1_1

---
+++ day1_1.php
@@ -0,0 +1,9 @@
+<?php
+$input = 'L1, R3, R1, L5, L2, L5, R4, L2, R2, R2, L2, R1, L5, R3, L4, L1, L2, R3, R5, L2, R5, L1, R2, L5, R4, R2, R2, L1, L1, R1, L3, L1, R1, L3, R5, R3, R3, L4, R4, L2, L4, R1, R1, L193, R2, L1, R54, R1, L1, R71, L4, R3, R191, R3, R2, L4, R3, R2, L2, L4, L5, R4, R1, L2, L2, L3, L2, L1, R4, R1, R5, R3, L5, R3, R4, L2, R3, L1, L3, L3, L5, L1, L3, L3, L1, R3, L3, L2, R1, L3, L1, R5, R4, R3, R2, R3, L1, L2, R4, L3, R1, L1, L1, R5, R2, R4, R5, L1, L1, R1, L2, L4, R3, L1, L3, R5, R4, R3, R3, L2, R2, L1, R4, R2, L3, L4, L2, R2, R2, L4, R3, R5, L2, R2, R4, R5, L2, L3, L2, R5, L4, L2, R3, L5, R2, L1, R1, R3, R3, L5, L2, L2, R5';
+$coords = [0,0];
+$facing = 1;
+foreach(explode(', ',$input) as $index => $step) {
+   $facing*=(($step[0]=='R'xor($index%2))?-1:1);
+   $coords[$index%2] += substr($step,1)*$facing;
+}
+echo array_sum(array_map('abs',$coords)), PHP_EOL;
